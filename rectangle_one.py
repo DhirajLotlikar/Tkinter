adb7040e81b417288fdd945397855931ea268453
SPengLiang <1243592003@qq.com>
2015-10-01 10:33:04 +0800

    rectangle

---
+++ rectangle_one.py
@@ -0,0 +1,20 @@
+from Tkinter import *
+tk = Tk()
+canvas = Canvas(tk, width = 500, height = 500)
+canvas.pack()
+canvas.create_line(0, 0, 500, 500)
+
+import random
+def random_rectangle(width, height):
+    x1 = random.randrange(width)
+    y1 = random.randrange(height)
+    x2 = x1 + random.randrange(width)
+    y2 = y1 + random.randrange(height)
+    canvas.create_rectangle(x1, y1, x2, y2)
+    
+for x in range(0, 100):
+	  random_rectangle(400, 400)
+	  
+a = raw_input("come on:")
+print(a)
+
