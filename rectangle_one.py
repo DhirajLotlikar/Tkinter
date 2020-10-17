be404d44a5edf2f188fbbc21198f6da5db6dc923
HemantL04 <73017628+HemantL04@users.noreply.github.com>
2020-10-17 14:48:11 +0530

    Update rectangle_one.py

--- rectangle_one.py
+++ rectangle_one.py
@@ -1,8 +1,8 @@
 from Tkinter import *
-root= Tk()
-canvas = Canvas(root, width = 1000, height = 1000)
+tk= Tk()
+canvas = Canvas(tk, width = 500, height = 500)
 canvas.pack()
-canvas.create_line(0, 0, 1000, 1000)
+canvas.create_line(0, 0, 500, 500)
 
 import random
 def random_rectangle(width, height):
@@ -12,7 +12,7 @@ def random_rectangle(width, height):
     y2 = y1 + random.randrange(height)
     canvas.create_rectangle(x1, y1, x2, y2)
     
-for x in range(0, 100):
+for x in range(0, 50):
 	  random_rectangle(400, 400)
 	  
 root.mainloop()
